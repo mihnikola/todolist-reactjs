2e246faa1819c93fef0542bcf5c4d42dadf7bb6c
mihnikola <47032376+mihnikola@users.noreply.github.com>
2024-10-21 14:01:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const persistData = (newData) => {
+    localStorage.setItem("todos", JSON.stringify({ todos: newData }));
+  };
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
+  const handleAddTodos = (newTodo) => {
+    const newList = [...todos, newTodo];
+    persistData(newList);
+
+    setTodos(newList);
+  };
+
+  const handleEditTodos = (index) => {
+    const valueEdit = todos[index];
+    setTodoValue(valueEdit);
+    handleDeleteTodos(index);
+  };
+
+  const handleDeleteTodos = (index) => {
+    const newList = [...todos];
+    const newFilterData = newList.filter((_, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newFilterData);
+
+    setTodos(newFilterData);
+  };
+
+  return (
+    <>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        handleDeleteTodos={handleDeleteTodos}
+        handleEditTodos={handleEditTodos}
+        todos={todos}
+      />
+    </>
+  );
+}
+
+export default App;
